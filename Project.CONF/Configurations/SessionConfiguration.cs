339a957152a8643c1355ac8b79c4d7b6a3eafa0a
Ahmet Terzioğlu <Yevmen63@gmail.com>
2024-07-29 16:36:08 +0300

    Conf katmanı tamamlandı.

---
+++ Project.CONF/Configurations/SessionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.ENTITIES.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.CONF.Configurations
+{
+    public class SessionConfiguration : BaseConfiguration<Session>
+    {
+        public override void Configure(EntityTypeBuilder<Session> builder)
+        {
+            base.Configure(builder);
+            builder.Property(x => x.Price).HasColumnType("money");
+        }
+    }
+}
